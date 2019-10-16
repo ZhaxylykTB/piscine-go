fcea477b2e5d31d45bbd09ad0e323073c6238275
ZhaxylykTB <ZhaxylykTB@gmail.com>
2019-10-16 20:41:59 +0600

    fmt

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,10 +4,10 @@ import "github.com/01-edu/z01"
 
 func main() {
 
-    i := 0
-    for i < 26 {
-	z01.PrintRune(rune(97 + i))
-	i++
-    }
-    z01.PrintRune('\n')
+	i := 0
+	for i < 26 {
+		z01.PrintRune(rune(97 + i))
+		i++
+	}
+	z01.PrintRune('\n')
 }
