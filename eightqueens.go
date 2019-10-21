4b09f8310f2a4e24af3ecf39d8d851e4e5449ab6
ZhaxylykTB <ZhaxylykTB@gmail.com>
2019-10-21 20:48:26 +0600

    eightqueens.go

--- eightqueens.go
+++ eightqueens.go
@@ -1,41 +1,59 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
-const N = 8
+const n int = 8
 
-var position = [N]int{}
+func EightQueens() {
+	var rows [n]int
+	getQueens(rows, 0)
+}
 
-func talivre(queennumber, rowposition int) bool {
-	for i := 0; i < queennumber; i++ { //verifica todas as rainhas e as suas posicoes para entao avancar
-		other_row_pos := position[i] //vai buscar a posicao de outra rainha
+func getQueens(rows [n]int, a int) {
+	for i := 0; i < n; i++ {
+		if checkPlace(rows, i, a) {
+			rows[a] = i
+			if a == n-1 {
+				for j := 0; j < n; j++ {
+					z01.PrintRune(getRunesForChess(rows[j]))
+				}
+				z01.PrintRune('\n')
+			} else {
+				getQueens(rows, a+1)
+			}
+		}
+	}
+}
 
-		if other_row_pos == rowposition || //mesma linha
-			other_row_pos == rowposition-(queennumber-i) || //diagonais
-			other_row_pos == rowposition+(queennumber-i) {
+func checkPlace(rows [n]int, a, b int) bool {
+	if b == 0 {
+		return true
+	}
+	for i := 0; i < b; i++ {
+		if rows[i] == a || rows[i] == a+b-i || rows[i] == a-b+i {
 			return false
 		}
 	}
 	return true
 }
 
-func resolverpuzzle(k int) {
-
-	if k == N {
-		for i := 0; i < N; i++ { //gera as possibilidades
-			fmt.Print(position[i] + 1)
-		}
-		fmt.Print("\n")
-	} else {
-		for i := 0; i < N; i++ {
-			if talivre(k, i) { //verifica se ta livre antes de colocar a rainha na posicao k
-				position[k] = i
-				//coloca outra rainha
-				resolverpuzzle(k + 1)
-			}
-		}
+func getRunesForChess(ch int) rune {
+	switch ch {
+	case 0:
+		return '1'
+	case 1:
+		return '2'
+	case 2:
+		return '3'
+	case 3:
+		return '4'
+	case 4:
+		return '5'
+	case 5:
+		return '6'
+	case 6:
+		return '7'
+	default:
+		return '8'
 	}
-}
-func EightQueens() {
-	resolverpuzzle(0)
 }
