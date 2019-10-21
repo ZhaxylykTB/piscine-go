17b271adff61bffde530b53f34c773cf3f86756e
ZhaxylykTB <ZhaxylykTB@gmail.com>
2019-10-21 20:45:15 +0600

    eightqueens.go

--- eightqueens.go
+++ eightqueens.go
@@ -7,11 +7,11 @@ const N = 8
 var position = [N]int{}
 
 func talivre(queennumber, rowposition int) bool {
-	for i := 0; i < queennumber; i++ {
-		other_row_pos := position[i]
+	for i := 0; i < queennumber; i++ { //verifica todas as rainhas e as suas posicoes para entao avancar
+		other_row_pos := position[i] //vai buscar a posicao de outra rainha
 
-		if other_row_pos == rowposition ||
-			other_row_pos == rowposition-(queennumber-i) ||
+		if other_row_pos == rowposition || //mesma linha
+			other_row_pos == rowposition-(queennumber-i) || //diagonais
 			other_row_pos == rowposition+(queennumber-i) {
 			return false
 		}
@@ -22,15 +22,15 @@ func talivre(queennumber, rowposition int) bool {
 func resolverpuzzle(k int) {
 
 	if k == N {
-		for i := 0; i < N; i++ {
+		for i := 0; i < N; i++ { //gera as possibilidades
 			fmt.Print(position[i] + 1)
 		}
 		fmt.Print("\n")
 	} else {
 		for i := 0; i < N; i++ {
-			if talivre(k, i) {
+			if talivre(k, i) { //verifica se ta livre antes de colocar a rainha na posicao k
 				position[k] = i
-
+				//coloca outra rainha
 				resolverpuzzle(k + 1)
 			}
 		}
