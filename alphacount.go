3b6b245c830ca8808c74b3c2bc76866fe326c17c
ZhaxylykTB <ZhaxylykTB@gmail.com>
2019-10-22 18:17:18 +0600

    alphacount.go

---
+++ alphacount.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AlphaCount(str string) int {
+    count := 0
+    for i := 0; i < len(str); i++ {
+        count++
+        if !(str[i] >= 'A') && (str[i] <= 'Z') && !(str[i] >= 'a') && (str[i] <= 'z') {
+            count--
+        }
+    }
+    return count
+}
