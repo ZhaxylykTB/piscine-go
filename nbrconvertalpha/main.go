bf09508d3c47fc44525bd61f27f4a9331e304c33
ZhaxylykTB <ZhaxylykTB@gmail.com>
2019-10-23 18:50:18 +0600

    nbrconvertalpha

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -1,107 +1,118 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
-func Length(str []rune) int {
-	c := 0
-	for i := range str {
-		if i == i {
-			c++
-		}
-	}
-	return c
-}
-
-func Sol(digit rune) int {
-	x := 0
-	for i := '1'; i <= digit; i++ {
-		if i == i {
-			x++
-		}
-	}
-	return x
-}
-
-func Sol2(d1, d2 rune) int {
-	x := 0
-	y := 0
-	for i := '1'; i <= d1; i++ {
-		if i == i {
-			x++
-		}
-	}
-	for i := '1'; i <= d2; i++ {
-		if i == i {
-			y++
-		}
-	}
-	return x*10 + y
-}
-
-func Solut(Arune []rune, len int, i int) []int {
-	var arr = []int{0, 0}
-	c := 1
-	if Arune[i+1] != 32 {
-		c++
-	}
-	if c == 2 {
-		if Arune[i] >= '1' && Arune[i] <= '9' && Arune[i+1] >= '0' && Arune[i+1] < '9' {
-			arr[1] = i + 2
-			arr[0] = Sol2(Arune[i], Arune[i+1])
-			return arr
-		}
-
-	} else {
-		if Arune[i] >= '1' && Arune[i] <= '9' {
-			arr[1] = i + 1
-			arr[0] = Sol(Arune[i])
-			return arr
-		}
-	}
-	return arr
-}
-
 func main() {
-	args := os.Args
-	s := ""
-	for i, v := range args {
-		if args[i] == "--upper" {
-			s = " " + s
-			continue
-		}
-		if i > 0 {
-			s += v + " "
-		}
+	arguments := os.Args
+	len := 0
+	table := []string(arguments[1:])
+	for index := range table {
+		len = index + 1
 	}
-	Arune := []rune(s)
-	len := Length(Arune)
-	for i := 0; i < len-1; i++ {
-		if args[1] == "--upper" {
-			if Arune[i] == ' ' {
-				z01.PrintRune(' ')
-				continue
+	if len > 0 {
+		if table[0] == "--upper" {
+			z01.PrintRune(' ')
+			tableUpp := []string(table[1:])
+			for _, num := range tableUpp {
+				if Atoi(num) >= 1 && Atoi(num) <= 26 {
+					z01.PrintRune(rune(Atoi(num) + 64))
+				} else {
+					z01.PrintRune(' ')
+				}
 			}
-			k := Solut(Arune, len, i)
-			if k[0] > 26 {
-				z01.PrintRune(32)
-			} else {
-				z01.PrintRune(rune(97 + k[0] - 33))
-			}
-			i = k[1]
-		} else if Arune[i] >= '0' && Arune[i] <= '9' {
-			k := Solut(Arune, len, i)
-			if k[0] > 26 {
-				z01.PrintRune(32)
-			} else {
-				z01.PrintRune(rune(97 + k[0] - 1))
-			}
-			i = k[1]
 		} else {
-			z01.PrintRune(32)
+			for _, num := range table {
+				if Atoi(num) >= 1 && Atoi(num) <= 26 {
+					z01.PrintRune(rune(Atoi(num) + 96))
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
 		}
 	}
 	z01.PrintRune('\n')
+}
+
+func BToInt(num byte) int {
+	var runenum int
+	switch num {
+	case 48:
+		runenum = 0
+	case 49:
+		runenum = 1
+	case 50:
+		runenum = 2
+	case 51:
+		runenum = 3
+	case 52:
+		runenum = 4
+	case 53:
+		runenum = 5
+	case 54:
+		runenum = 6
+	case 55:
+		runenum = 7
+	case 56:
+		runenum = 8
+	case 57:
+		runenum = 9
+	}
+	return runenum
+}
+
+func SLen(str string) int {
+	var count int
+	var a int
+	for index, word := range str {
+		if word == 233 {
+			a = -1
+		}
+		count = index + 1 + a
+	}
+	return count
+}
+
+func BasicAtoi2(s string, ind int) int {
+	lenth := SLen(s)
+	var num int = 0
+	str := []byte(s)
+	for i := ind; i < lenth; i++ {
+		if str[i] > 47 && str[i] < 58 {
+			for j := i; j < lenth; j++ {
+				if str[j] > 47 && str[j] < 58 {
+					num = num*10 + BToInt(s[j])
+				} else {
+					return 0
+				}
+			}
+			break
+		} else {
+			break
+		}
+
+	}
+	return num
+}
+
+func Atoi(s string) int {
+	str := []byte(s)
+	var num int
+	if SLen(s) >= 2 {
+
+		if str[0] == 43 {
+			num = BasicAtoi2(s, 1)
+		} else if str[0] == 45 {
+			num = (-1) * BasicAtoi2(s, 1)
+		} else {
+			num = BasicAtoi2(s, 0)
+		}
+	} else {
+		num = BasicAtoi2(s, 0)
+	}
+
+	return num
 }
